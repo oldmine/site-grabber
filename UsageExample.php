9cccbe7abb90a6714b9f63e38b473dbd2c5ae89e
Zhelninskiy Daniil <webmailexec@gmail.com>
2018-12-26 20:09:30 +0400

    Reformatted usage example files
    Signed-off-by: Zhelninskiy Daniil <webmailexec@gmail.com>

--- UsageExample.php
+++ UsageExample.php
@@ -13,17 +13,22 @@ require './vendor/autoload.php';
 
 //Site Index URL when we want to parse
 $indexUrl = '';
+
 //Create simple handler that save page to database
 $handler = new SaveToDatabasePageHandler();
+
 //Get already handled page, if you want to stop parse and resume from old position
 //$savedPosts = $handler->getSavedPages(parse_url($indexUrl)['host']);
 
 //Create visit strategy and provide new Data Loader
 $visitStrategy = new RecursivelyVisitStrategy($indexUrl, new CurlDataLoader());
+
 //Create grabber which use provided visit strategy
 $grabber = new Grabber($visitStrategy);
+
 //Add page handler
 $grabber->addPageHandler($handler);
+
 //Run grabber
 $grabber->start();
 //$grabber->startFromOldPosition($savedPosts);
